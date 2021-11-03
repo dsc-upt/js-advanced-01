357de99a6f1e7f5a844043ac20e6b18928fd8e5a
Miri <mirialin.39@gmail.com>
2021-11-03 21:21:08 +0200

    added dict

--- Miri Marius-Alin/Miri Marius-Alin.js
+++ Miri Marius-Alin/Miri Marius-Alin.js
@@ -61,3 +61,62 @@ for (const item of myArray){
     console.log(item);   
 }
 console.log();
+
+let unsorted = [2, 4, 1, 5, 3, 6, 4, 5];
+// let ok = 1;
+// while( ok ){
+//     let aux;
+//     ok = 0;
+//     for ( let i = 0; i < unsorted.length - 1; i++){
+//     if (unsorted[i] > unsorted [i+1]){
+//         aux = unsorted[i];
+//         unsorted [i] = unsorted [i + 1];
+//         unsorted[ i + 1 ] = aux;
+//         ok = 1; 
+//     }
+// }
+// }
+
+// nu se modifica parametrii unei functii
+console.log();
+function decrement ( number ) {
+    const newNumber = number - 1;
+    console.log(newNumber);
+    if(newNumber > 1){
+        return decrement (newNumber);
+    }
+    return;
+}
+
+decrement ( 10 );
+
+const dictionary = { 
+    one: 'one',
+    "two-two": [1, 5, 6],
+    4: "patru"
+}
+console.log(dictionary);
+console.log(dictionary.one);
+console.log(dictionary["two-two"]);
+console.log(dictionary[4]);
+dictionary["three"] = "number three";
+dictionary["one"] = "number 45";
+console.log(dictionary.three);
+console.log(dictionary.one);
+
+for ( const key in dictionary){
+    console.log(key, dictionary[key]);
+}
+
+console.log();
+const keys = Object.keys(dictionary);
+console.log(keys);
+
+for (const key of keys){
+    console.log(key);
+}
+
+dictionary["showName"] = showName;
+
+dictionary.showName("Name from dic");
+dictionary["showName"]("Name from dic v2.0");
