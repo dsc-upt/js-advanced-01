bdc59bb82ac816d9fe67cfa80c2a37bfe19f02a7
Norbi76 <kondora35@gmail.com>
2021-11-03 20:25:53 +0300

    ceva_cod

---
+++ ex1.js
@@ -0,0 +1,16 @@
+function myFunc(par1, par2) {
+    console.log(par1 + par2);
+}
+
+myFunc("ceva", " altceva");
+
+const showName = function (name){
+    console.log(name);
+}
+
+showName("nrbi");
+
+const myLambda = (name) =>{
+    console.log("alt ceva " + name);
+}
+myLambda("ceva");
