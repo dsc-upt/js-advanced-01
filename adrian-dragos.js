17e542167d82db551ae0967df728ec529ba0cdbb
adrian-dragos <adrian123795@gmail.com>
2021-11-03 20:52:11 +0200

    Made all kinds of for loops in js

--- adrian-dragos.js
+++ adrian-dragos.js
@@ -31,3 +31,26 @@ function myFunc3(Name, ...params) {
 }
 
 myFunc3("lala", 1, 4, 6, 101)
+
+
+const myArray = [1, 5, 10, 15, 20, 25]
+console.log(myArray.filter(n => n % 3 === 0))
+console.log(myArray.filter(function (value, index) {
+    return myArray[index] % 3 === 0
+}))
+
+
+
+// loop array
+for (let i = 0; i < myArray.length; i++) {
+    console.log(myArray[i] * myArray[i])
+}
+
+// loop array lambda
+myArray.forEach(value => console.log(value * 100))
+
+// loop array foreach
+for (const item of myArray)
+    console.log(item - item)
+
+
