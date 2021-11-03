03662c1b7f3a1cf9f883d6936e49d7c4afa7f135
Cristian Canceal <cristicanceal@gmail.com>
2021-11-03 20:52:24 +0200

    worked with arrays

--- canceal.js
+++ canceal.js
@@ -20,3 +20,23 @@ const myLambda = (name = 'GDSC', ...params) => {
 }
 
 myLambda('Dan', 1, 3, 4, 5, 6, 2, 1);
+
+const myArray = [1, 2, 3, 40, 50, 23, 45];
+
+function by10(value, index) {
+    console.log(value, index);
+    return value % 10 === 0;
+}
+console.log('');
+
+console.log(myArray.filter(function(value, index){
+    return value % 10 === 0;
+}));
+
+console.log(myArray.filter(value => value % 10 === 0));
+
+myArray.forEach((value, index) => console.log(value * 10 + index));
+
+for (const item of myArray) {
+    console.log(item);
+}
