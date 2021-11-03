063600f27145b8043243513960fc320c051de8a2
Cristian Canceal <cristicanceal@gmail.com>
2021-11-03 21:21:11 +0200

    worked with dict

--- canceal.js
+++ canceal.js
@@ -40,3 +40,49 @@ myArray.forEach((value, index) => console.log(value * 10 + index));
 for (const item of myArray) {
     console.log(item);
 }
+
+function decrement(number) {
+    //number = number--; bad programming practice
+    const newNumber = number - 1;
+    console.log(newNumber);
+    if(newNumber > 1) {
+        return decrement(newNumber);
+    }
+    return;
+}
+
+decrement(10);
+
+const dictionary = {
+    one: 'one',
+    "two-two" : [1, 5, 6],
+    "number" : [1, 1, 1],
+    4: "patru"
+}
+
+console.log(dictionary);
+console.log(dictionary.one);
+console.log(dictionary.number);
+console.log(dictionary["two-two"]);
+console.log(dictionary[4]);
+dictionary["three"] = "number three";
+dictionary["number"] = "45";
+
+console.log(dictionary.three);
+console.log(dictionary);
+
+for (const key in dictionary) {
+    console.log(key, dictionary[key]);
+}
+
+const keys = Object.keys(dictionary);
+console.log(keys);
+
+for(const key of keys) {
+    console.log(key);
+}
+
+dictionary['showName'] = showName;
+
+dictionary.showName('Name from dict');
+dictionary['showName']('Name from dict v2');
