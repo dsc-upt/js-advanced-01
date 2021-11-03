fab2bd28ee35e498f13ccccf597cf4011dcfe393
Cristian <turcancristina02@gmail.com>
2021-11-03 20:57:36 +0200

    am pus si array-uri

---
+++ TurcanCristian/turcan-cristian-js2/main.js
@@ -0,0 +1,57 @@
+function myFunc(firstName, lastName = 'GDSC'){
+    console.log(firstName+ ' ' + lastName);
+}
+
+myFunc('My club is');
+
+//Anonymus function
+const showName= function (name = 'GDSC'){
+    console.log(name)
+}
+
+showName('Cristi')
+
+const myLambda = (name = 'GDSC', ...params) => {
+    console.log(name)
+    console.log(params)
+}
+
+myLambda('Cristi', 1,2,3,4,5)
+
+
+const myArray = [1, 2, 3, 40, 50, 23, 45]
+
+function by10(value, index) {
+    return value % 10 === 0;
+}
+
+console.log(myArray.filter(by10))
+console.log('')
+
+console.log(myArray.filter(function(value, ){
+    return value % 10 === 0;
+}))
+
+console.log(myArray.filter((value) => {
+    return value % 10 === 0
+}))
+
+console.log(myArray.filter(value => value % 10 ===0));
+
+myArray.forEach((value, index ) => console.log(value * 10 + index));
+// myArray.forEach(value => {
+//     const x = value + 5;
+//     const y = value + 106;
+//
+//
+//
+//     return;
+// });
+
+for (const item of myArray){
+    if (item === 50){
+        break;
+    }
+
+    console.log(item)
+}
