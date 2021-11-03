738d214f7d6e5cba7abf3b11efc0642b4ab22f25
dcarpencu <dcarpencu@gmail.com>
2021-11-03 21:21:16 +0200

    David a facut dictionare

--- dcarpencu.js
+++ dcarpencu.js
@@ -59,3 +59,48 @@ for (const item of myArray){
     console.log(item);
 }
 
+//FUNCTII RECURSIVE
+
+function decrement(number){
+    //number = number--; -> bad progrmming practice
+    const newNumber = number-1;
+    console.log(newNumber);
+
+    if(newNumber > 1){
+        return decrement(newNumber)
+    }
+    return;
+}
+
+decrement(10)
+
+const dictionary = {
+    one: 'one',
+    "two-two": [1, 5, 6],
+    "number": [1, 5, 6],
+    4: "patru"
+}
+// interzis const bb-bb = 0
+
+console.log(dictionary)
+console.log(dictionary.one)
+console.log(dictionary["two-two"])
+console.log(dictionary[4])
+dictionary["three"] = "number three"
+dictionary["number"] = "45"
+
+console.log(dictionary.three)
+console.log(dictionary)
+
+for (const key in dictionary){
+    console.log(key, dictionary[key])
+}
+
+for(const key of Object.keys(dictionary)){
+    console.log(key)
+}
+
+dictionary['showName'] = showName
+
+dictionary.showName('Name from dict')
+dictionary['showName']('Name from dict v2.0')
