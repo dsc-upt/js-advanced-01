c0ed6cbb4a4beff93887bd2d744e174be21c8800
Denisa Pop <xdxeyny1612@gmail.com>
2021-11-03 20:57:17 +0200

    worked with arrays correct

--- ws.js
+++ ws.js
@@ -25,29 +25,29 @@ myLambda('Dan', 1,2,3,5,5,2)
 const myArray = [1, 2, 3, 40, 50, 23, 45]
 
 function by10(value, index) {
-    console.log(value, inde)
-    return value % 10 == 0;
+    console.log(value, index)
+    return value % 10 === 0;
 }
 
 console.log(myArray.filter(by10))
 console.log('')
 
     console.log(myArray.filter(function (value, index) {
-        return value % 10 == 0;
+        return value % 10 === 0;
 }))
 
 console.log(myArray.filter((value ) => {
-    return value % 10 == 0;
+    return value % 10 === 0;
 
 }))
 
-console.log(myArray.filter(value => value % 10 == 0 ))
+console.log(myArray.filter(value => value % 10 === 0 ))
 
 myArray.forEach((value , index)  => console.log(value * 10 + index));
 
 for (const item of myArray)
 {
-    if(item == 50)
+    if(item === 50)
     {
         break;
     }
