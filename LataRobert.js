5499dd2ef90db348a6ea7d1084fe9bc3c00c62bd
Robert.Lata <robert.lata@student.upt.ro>
2021-11-03 21:21:15 +0200

    Am lucrat cu dictionare

--- LataRobert.js
+++ LataRobert.js
@@ -30,15 +30,15 @@ const myarray = [1, 2, 3, 40, 50, 23, 45];
 
 function by10(value, index) {
     console.log(value, index);
-    return value % 10 == 0;
+    return value % 10 === 0;
 }
 
 console.log(myarray.filter(by10));
 console.log(myarray.filter(function(value, index) {
-    return value % 10 == 0;
+    return value % 10 === 0;
 }));
 
-console.log(myarray.filter(value => value % 10 == 0));
+console.log(myarray.filter(value => value % 10 === 0));
 
 myarray.forEach( (value, index) => console.log(value * 10 + index));
 
@@ -47,7 +47,49 @@ myarray.forEach( (value, index) => console.log(value * 10 + index));
 //}
 
 for (const item of myarray) {
-    if(item == 50)
+    if(item === 50)
         break;
     console.log(item);
 }
+
+function decrement (number) {
+    //number = number--; ->asa nu
+    const newNumber = number - 1;
+    console.log(newNumber);
+    if(newNumber > 1) {
+        return decrement(newNumber);
+    }
+    return;
+}
+decrement(10);
+
+const myDictionary = {
+    one: 'one',
+    "two-two": [1, 5, 6],
+    "numbers": [1, 5, 6],
+    4: "patru"
+}
+console.log(myDictionary);
+console.log(myDictionary.one);
+console.log(myDictionary.numbers);
+console.log(myDictionary["two-two"]);
+console.log(myDictionary[4]);
+myDictionary["three"] = "number 3";
+console.log(myDictionary.three);
+myDictionary["number"]= "45";
+console.log(myDictionary.numbers);
+
+for (const key in myDictionary) {
+    console.log(key, myDictionary[key]);
+}
+const keys = Object.keys(myDictionary);
+console.log(keys);
+
+for (const key of Object.keys(myDictionary)) {
+    console.log(key);
+}
+
+myDictionary['showName'] = showName
+myDictionary.showName('Name from dictionary');
+myDictionary['showName']('Name from dictionary v2');
+
