deec7b241e15a7a581b2f259c9a1e4b96b000664
Robert.Lata <robert.lata@student.upt.ro>
2021-11-03 20:51:58 +0200

    Am lucrat cu vectori

--- LataRobert.js
+++ LataRobert.js
@@ -25,3 +25,29 @@ const myLambda = (name='gdsc', ...params) => {
     console.log(params);
 }
 myLambda('Roby', 1, 2, 3);
+
+const myarray = [1, 2, 3, 40, 50, 23, 45];
+
+function by10(value, index) {
+    console.log(value, index);
+    return value % 10 == 0;
+}
+
+console.log(myarray.filter(by10));
+console.log(myarray.filter(function(value, index) {
+    return value % 10 == 0;
+}));
+
+console.log(myarray.filter(value => value % 10 == 0));
+
+myarray.forEach( (value, index) => console.log(value * 10 + index));
+
+//for (const item of myarray) {
+   //console.log(item);
+//}
+
+for (const item of myarray) {
+    if(item == 50)
+        break;
+    console.log(item);
+}
