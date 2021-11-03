5c2aa44ee81cadac50f07d404e2bab68651cf7f2
dcarpencu <dcarpencu@gmail.com>
2021-11-03 20:51:45 +0200

    David a lucrat cu areiuri

--- dcarpencu.js
+++ dcarpencu.js
@@ -5,8 +5,8 @@ function myFunc(firstName, lastName) {
 myFunc('David', 'Carpencu');
 
 // Anonymous function
-const showName = function (name){
-    console.log(name = 'GDSC')
+const showName = function (name = 'GDSC'){
+    console.log(name)
 }
 
 showName('Dan')
@@ -16,4 +16,46 @@ const myLambda = (name = 'GDSC', ...params) => {
     console.log(params)
 }
 
+myLambda(45)
 myLambda('Dan', 1, 2, 3, 4, 5, 2)
+
+const myArray = [1, 2, 3, 40, 50, 23, 45]
+
+function by10(value, index){
+    console.log(value, index)
+    return value % 10 === 0;
+}
+
+console.log(myArray.filter(by10))
+console.log(myArray.filter(function(value, index){
+    return value % 10 === 0;
+}))
+
+console.log(myArray.filter((value) =>{
+    return value % 10 === 0
+}))
+
+console.log(myArray.filter(value => value % 10 === 0));
+
+myArray.forEach(value => console.log(value * 10));
+
+myArray.forEach((value, index) => console.log(value * 10 + index));
+
+// myArray.forEach(value => {
+//     const x = value + 5;
+//     const y = value + 106;
+//
+//     return;
+// });
+
+for (const item of myArray){
+    console.log(item);
+}
+for (const item of myArray){
+    if (item === 50){
+        break;
+    }
+
+    console.log(item);
+}
+
