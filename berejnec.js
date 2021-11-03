a6ff9cfcd5feaf15cbd25a57622ddc2425cf2f05
Adrian Berejnec <adrianberejnec29@gmail.com>
2021-11-03 20:51:50 +0200

    Learned Arrays

--- berejnec.js
+++ berejnec.js
@@ -16,3 +16,30 @@ const myLambda = (name = "GDSC", ...params) => {
 }
 
 myLambda("Adi",23,34,56,7)
+
+
+const myArray = [1, 2, 3, 40, 23 , 45, 89, 90]
+
+function by10(value, index) {
+    console.log(value, index)
+    return value % 10 === 0;
+}
+
+console.log(myArray.filter(by10))
+console.log(myArray.filter(function(value, index){
+    return value % 10 === 0;
+}))
+
+console.log(myArray.filter((value) => {
+    return value % 10 === 0
+}))
+
+console.log(myArray.filter(value => value % 10 === 0))
+
+myArray.forEach((value, index) => console.log(value * 10 + index));
+
+for (const item of myArray){
+    if(item === 23)
+        break;
+    console.log(item)
+}
