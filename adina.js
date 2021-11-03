484e77b0e9cd0472ea8fd41e11ae90005a2efbbf
Adina Cojocar <adinacojocar17@gmail.com>
2021-11-03 20:29:15 +0200

    comment

---
+++ adina.js
@@ -0,0 +1,15 @@
+// const x=undefined;
+function myFunct(firstName, lastName= 'GDSC')
+{
+    console.log(firstName+ ' '+ lastName);
+}
+myFunct('My club is');
+const showName= function(name)
+{
+    console.log(name)
+}
+showName('Adina')
+const myLambda =() =>
+{
+    console.log('myLambda')
+}
