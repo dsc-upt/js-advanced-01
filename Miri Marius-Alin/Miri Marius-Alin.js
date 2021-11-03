b4943cf1b48a1228f0ad5febaae4d3850dbdde1a
Miri <mirialin.39@gmail.com>
2021-11-03 20:51:43 +0200

    added arrayz

--- Miri Marius-Alin/Miri Marius-Alin.js
+++ Miri Marius-Alin/Miri Marius-Alin.js
@@ -16,3 +16,48 @@ const myLambda = (name = "GDSC" , ...params) => {
 
 myLambda ("Miri", 1,2,true,"4");
 
+function stopAtFive(){
+    let n;
+    while( n !== 5)
+    {
+        n = Math.floor(Math.random() * 10 + 1);
+        console.log(n);
+    }
+}
+
+stopAtFive();
+
+const myArray = [1, 3, 40, 5, 60, 2];
+function by10(value, index){
+    console.log(value, index);
+    return value % 10 === 0;
+}
+
+console.log( myArray.filter(by10) );
+
+console.log(myArray.filter(function(value, index){
+    return value % 10 === 0;
+}));
+
+console.log(myArray.filter(value => value % 10 === 0));
+console.log();
+
+myArray.forEach(value => console.log(value*10));
+console.log();
+
+myArray.forEach ((value, index) => console.log(value * 10 + index));
+console.log();
+
+for (const item of myArray){
+    console.log(item);   
+}
+console.log();
+
+for (const item of myArray){
+    if (item === 40)
+    {
+        break;
+    }
+    console.log(item);   
+}
+console.log();
