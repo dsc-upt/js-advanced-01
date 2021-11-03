42c7aef17130392b845dce7729443dbd98f55c65
Adina Cojocar <adinacojocar17@gmail.com>
2021-11-03 20:53:17 +0200

    test

--- adina.js
+++ adina.js
@@ -13,3 +13,28 @@ const myLambda =() =>
 {
     console.log('myLambda')
 }
+const myArray=[1, 2, 3, 40, 50, 23, 45]
+
+function by10(value, index)
+{
+    console.log(value, index){
+    return value %10==0}
+}
+console.log(myArray.filter(by10))
+console.log(' ')
+
+console.log(myArray.filter(function(value, index))) {
+    return value %10==0
+}
+console.log(myArray.filter(value=>value %10==0));
+
+myArray.forEach((value, index)=>console.log(value *10));
+
+
+
+for (const item of myArray)
+{
+    if(item==50)
+        break;
+    console.log(item)
+}
