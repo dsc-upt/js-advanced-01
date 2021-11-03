bc6e984de8967fd9283d6b8c80aeff7c78baa57b
Norbert <kondora35@gmail.com>
2021-11-03 20:38:39 +0300

    ceva_cod

--- ex1.js
+++ ex1.js
@@ -14,3 +14,11 @@ const myLambda = (name) =>{
     console.log("alt ceva " + name);
 }
 myLambda("ceva");
+
+const myArray = [20,10,3,40,5,6];
+
+function by10(value, index){
+    return value % 10 == 0;
+}
+
+console.log(myArray.filter(by10));
